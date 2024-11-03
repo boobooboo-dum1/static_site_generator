a700868e6c216b86373cf6f8d3ca032b2fef4b9a
Emmanuelle Quilliet <emmanuelle.quilliet@gmai.com>
2024-11-03 15:48:29 +0100

    feat: Add copy_fs_tree func

--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
-print("hello world")
+import shutil
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_DIR = BASE_DIR / "static"
+PUBLIC_DIR = BASE_DIR / "public"
+
+
+def copy_fs_tree(source: Path, destination: Path):
+    if destination.exists and destination.is_dir:
+        shutil.rmtree(destination)
+
+    shutil.copytree(source, destination)
+
+
+def main():
+    copy_fs_tree(STATIC_DIR, PUBLIC_DIR)
+
+
+if __name__ == "__main__":
+    main()
